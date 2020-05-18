a6b1d93b83906755fa9c104acc211582d4718f32
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-05-18 09:38:52 +0530

    TODO list task with react, designing the funtion and designing the html part

--- todo-app.js
+++ todo-app.js
@@ -1,74 +1,23 @@
-let taskList = []
+function Task(props) {
+    console.log("task dsiplay")
+    return <li
+    style={{
 
-class Task {
-    constructor(name, dueDate, isDone) {
-        this.taskId = Date.now();
-        this.name = name;
-        this.dueDate = dueDate;
-        this.isDone = isDone;
-    }
-
-    toString() {
-        let htmlText = '<li class="task" ><div>'
-        htmlText += this.name
-        htmlText += ", " + this.dueDate.getDate() 
-                 + "/" + this.dueDate.getMonth();
-        htmlText += '<input type="checkbox" name="isDone" id="isDone">'
-        htmlText += '<button onclick="deleteTask(';
-        htmlText += this.taskId;
-        htmlText += ')">Delete</button>';
-        htmlText += '</div></li>';
-        return htmlText;
-    }
-}
-
-function render() {
-    const listUI = document.getElementById("todolist")
-    listUI.innerHTML = "";
-    if (taskList.length === 0) listUI.innerHTML = "No tasks todo :-)"
-    taskList.forEach((task) => {
-        listUI.innerHTML += task.toString();
-    })
-}
-
-function deleteTask(taskId) {
-    taskList = taskList.filter(
-        (t) => {
-            if(t.taskId != taskId) 
-            return t;
-        }
-    );
-    // call a web api to update the database on the server
+        color: props.task.status ? "red" : "black"
+        
+    }}
+>
+        
     
-    // update the DOM
-    render()
-    console.log(taskList);
+    {props.name}, {props.dueDate.toLocaleTimeString()} ,{' '}
+
+    <input type = "checkbox" onChange = {() =>{
+        props.onChange(props.task.id);
+    }}
+    />
+    <button value = "Delete" onClick = {() =>
+        props.onDelete(props.task.id)
+    }>Delete</button>
+    
+    </li>
 }
-
-function createTask() {
-    const taskName = document.getElementById("taskName").value;
-    addTask(new Task(taskName, new Date(), false));
-}
-
-function addTask(t) {
-    taskList.push(t)
-    // call a web api to update the database on the server
-    render();
-    console.log(taskList)
-}
-
-function init() {
-    console.log("init called");
-
-    // call a web api to retrieve the task list
-    // write a function to send a api request
-    // get the JSON
-    // assign it to taskList
-    // render
-
-    task = new Task("welcome task", new Date("May 30, 2020"), false);
-    addTask(task);
-    console.log(task);
-}
-
-init();
